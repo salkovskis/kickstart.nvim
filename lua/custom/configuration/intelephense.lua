9650532ce6af01ea2eab054254b9e3f4ba8f25d6
raivo.salkovskis <raivo@salkovskis.com>
2024-06-19 00:15:09 +0300

    intelephense not activating

--- lua/custom/configuration/intelephense.lua
+++ lua/custom/configuration/intelephense.lua
@@ -2,8 +2,14 @@ require('mason-tool-installer').setup { ensure_installed = { 'intelephense' }}
 
 require('lspconfig').intelephense.setup {
   settings = {
-    tabwidth = 4,
-    linenseKey = os.getenv("INTELEPHENSE_LICENSE_KEY") or '',
+    intelephense = {
+      tabwidth = 4,
+      -- This way of activating intelephense does not work
+      -- what works is creating $HOME_DIR/intelephense/licence.txt file
+      licenseKey = os.getenv("INTELEPHENSE_LICENSE_KEY") or '',
+    },
+  },
+  filetypes = {
+    "php"
   }
 }
-
